1d7dacdd7c0abcb0ea72152c8f7b648a53ea514e
jessesh <jessesh@microsoft.com>
2015-04-28 17:37:29 -0700

    no message

--- src/pages/jesse/jesse.ts
+++ src/pages/jesse/jesse.ts
@@ -1,11 +1,10 @@
 ﻿///<reference path="../../../typings/winjs.d.ts" />
-///<reference path="../../../typings/winjs-store.d.ts" />
 
 (() => {
-    window.addEventListener("DOMContentLoaded", () => {
-        WinJS.UI.processAll().then(() => {
-
-        });
+    WinJS.UI.Pages.define("/pages/search/search.html", {
+        ready: function (element, options) {
+        },
+        unload: function () {
+        }
     });
-
-});
+})();
