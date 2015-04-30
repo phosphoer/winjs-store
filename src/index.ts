addb9ad1860fbdab428973af7df1243748bdc999
Sean Xu <jseanxu@live.com>
2015-04-29 19:09:29 -0700

    More styling

--- src/index.ts
+++ src/index.ts
@@ -118,10 +118,7 @@ module Store {
 
 (() => {
     var categories = ["Electronics", "Kitchen", "Books", "Furniture", "Outdoor"];
-    var companies = [];
-    for (var i = 0; i < 10; i++) {
-        companies.push("Company " + String.fromCharCode(65 + i));
-    }
+    var companies = ["MoneyGrab Inc.", "Rip-Off Corporation", "Buy Lo Sell Hi LLC", "Cheap Stuff Market", "No Support Retailer"];
 
     var loremIpsum = [
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
